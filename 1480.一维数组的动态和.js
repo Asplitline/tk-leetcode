ea6098ccdb653f3ea097396f944cabe804d9041b
myqz <ambition292@163.com>
2022-07-27 23:40:24 +0800

    p: 1480

--- 1480.一维数组的动态和.js
+++ 1480.一维数组的动态和.js
@@ -2,4 +2,15 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var runningSum = function (nums) {}
+
+// https://leetcode.cn/problems/running-sum-of-1d-array/
+
+var runningSum = function (nums) {
+  const len = nums.length
+  const result = []
+  result[0] = nums[0]
+  for (let i = 1; i < len; i++) {
+    result[i] = result[i - 1] + nums[i]
+  }
+  return result
+}
