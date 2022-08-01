9e2ad43f7620baefe5311b6a394324eea0c3b2a1
Asplitline <893666612@qq.com>
2022-08-01 23:53:11 +0800

    lt: 724

--- 724.寻找数组的中心下标.js
+++ 724.寻找数组的中心下标.js
@@ -9,8 +9,31 @@
  * @param {number[]} nums
  * @return {number}
  */
-var pivotIndex = function(nums) {
-
-};
+const pivotIndex = function (nums) {
+  const first = []
+  const last = []
+  const len = nums.length
+  first[0] = 0
+  last[len - 1] = 0
+  for (let i = 1; i < len; i++) {
+    first[i] = first[i - 1] + nums[i - 1]
+  }
+  for (let i = len - 2; i >= 0; i--) {
+    last[i] = last[i + 1] + nums[i + 1]
+  }
+  let idx = -1
+  for (let v = 0; v < len; v++) {
+    if (first[v] === last[v]) {
+      idx = v
+      break
+    } else if (last[v] === 0 && v === 0) {
+      idx = 0
+      break
+    } else if (first[len - 1] === 0 && v === len - 1) {
+      idx = 0
+      break
+    }
+  }
+  return idx
+}
 // @lc code=end
-
